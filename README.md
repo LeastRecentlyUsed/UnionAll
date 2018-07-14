25ad4a1245f070d359bc8eba102e16c4772c0148
Andy Roberts <avrpvrt@gmail.com>
2018-07-14 17:19:45 +0100

    corrected Vector store.test methods

--- README.md
+++ README.md
@@ -2,10 +2,43 @@
 A REST API using a storage model comprised of 2 nodes with a vector.  Used to emulate triple stores or graph stores.
 
 
+## Use In Development
+Download or clone the project to a local directory.
 
-Net Core license information
-https://docs.microsoft.com/en-us/dotnet/core/
-https://dotnetfoundation.org/
+Download and install the 2.1 SDK
+https://www.microsoft.com/net/download/dotnet-core/sdk-2.1.300
 
-npgsql license information
-http://www.npgsql.org/#license
+### Create the database
+
+Open a command prompt.
+
+Navigate to the `UnionAll.Store` directory and set the development environment for EF Core.
+- `set ASPNETCORE_ENVIRONMENT=Development`
+
+| Optional |
+| -- |
+|Create the database migration files for a SQLite database.  Before generating a fresh DB migration script make sure to delete all the previous files from the `UnionAll.Store.Migrations` folder before executing this step |
+| `dotnet ef migrations add Sqlite --startup-project ../UnionAll.Api` |
+
+Execute the migration
+- `dotnet ef database update --startup-project ../UnionAll.Api`
+
+The database should exist as `UnionAll/UnionAll.Api/bunkerfile.db`
+
+To query or interact directly with the database contents, download the SQLite Browser:
+https://github.com/sqlitebrowser/sqlitebrowser/releases 
+
+### Build & Run
+
+Open a command prompt.
+
+Navigate to the `UnionAll.Api` directory, restore and build the applicaton.
+- `dotnet restore`
+- `dotnet build`
+- `dotnet run --launch-profile Dev`
+
+The application contains launch configuration for Dev, Stage and Prod.  Only the Dev configuration will use the SQLite database in EF Core. 
+
+The home directory of the project has a Postman Json file of API examples ("UnionAll.postman_collection.json").
+Postman can be downloaded from [here](https://www.getpostman.com/apps) .  
+
--- UnionAll.Store.Test/VectorDbOperationsShould.cs
+++ UnionAll.Store.Test/VectorDbOperationsShould.cs
@@ -13,9 +13,11 @@ namespace UnionAll.Store.Test
     {
         private static DbContextOptionsBuilder _opt;
         private static Node _node1;
-        private static Node _object1;
-        private static Node _root1;
-        private static Node _parent1;
+        private static Node _node2;
+        private static Node _node3;
+        private static Node _node4;
+        private static Node _node5;
+        private static Node _node6;
         private static DataRequestParams _reqParams;
 
         [ClassInitialize]
@@ -29,23 +31,35 @@ namespace UnionAll.Store.Test
                 NodeType = NodeValueTypes.Name,
                 NodeTopic = NodeTopics.Country
             };
-            _object1 = new Node
+            _node2 = new Node
             {
                 NodeName = "3",
                 NodeType = NodeValueTypes.Percentage,
                 NodeTopic = NodeTopics.Economic
             };
-            _root1 = new Node
+            _node3 = new Node
             {
-                NodeName = "Root",
+                NodeName = "Belgium",
                 NodeType = NodeValueTypes.Name,
                 NodeTopic = NodeTopics.Political
             };
-            _parent1 = new Node
+            _node4 = new Node
             {
-                NodeName = "Parent",
+                NodeName = "GDP",
                 NodeType = NodeValueTypes.Name,
                 NodeTopic = NodeTopics.Economic
+            };
+            _node5 = new Node
+            {
+                NodeName = "Employment",
+                NodeType = NodeValueTypes.Name,
+                NodeTopic = NodeTopics.Economic
+            };
+            _node6 = new Node
+            {
+                NodeName = "Canada",
+                NodeType = NodeValueTypes.Name,
+                NodeTopic = NodeTopics.Country
             };
 
             _reqParams = new DataRequestParams
@@ -65,22 +79,38 @@ namespace UnionAll.Store.Test
         public async Task SaveAndSelectOneVector()
         {
             int _vectorCount = 0;
+            int _nodeId = 0;
             Vector _saveResult;
             //arrange
             Vector _vector = new Vector
             {
                 VectorPhrase = "Empty Vector"
             };
-            //act
             using (var ctxSave = new DataContext(_opt.Options))
             {
-                VectorRepository _db = new VectorRepository(ctxSave);
+                NodeRepository _db = new NodeRepository(ctxSave);
+                await _db.InsertNodeAsync(_node1);
+            }
+            using (var ctxReader = new DataContext(_opt.Options))
+            {
+                VectorRepository _db = new VectorRepository(ctxReader);
+                _nodeId = (
+                    from n in ctxReader.Nodes
+                    where n.NodeName == "France"
+                    select n.NodeId
+                    ).FirstOrDefault();
+            }
+            _vector.NodeSubject = _nodeId;
+            //act
+            using (var ctxSaveVector = new DataContext(_opt.Options))
+            {
+                VectorRepository _db = new VectorRepository(ctxSaveVector);
                 _saveResult = await _db.InsertVectorAsync(_vector);
             }
-            using (var ctxRead = new DataContext(_opt.Options))
+            using (var ctxReadVector = new DataContext(_opt.Options))
             {
-                VectorRepository _db = new VectorRepository(ctxRead);
-                _vectorCount = (_db.SelectVectorAsync(1, _saveResult.VectorId) != null ? 1 : 0);
+                VectorRepository _db = new VectorRepository(ctxReadVector);
+                _vectorCount = (_db.SelectVectorAsync(_nodeId, _saveResult.VectorId) != null ? 1 : 0);
             }
             //assert
             Assert.AreEqual(1, _vectorCount);
@@ -92,29 +122,35 @@ namespace UnionAll.Store.Test
             Node _node;
             int _vectorCount = 0;
             //arrange
-            using (var ctxSave = new DataContext(_opt.Options))
+            using (var ctxSaveNode = new DataContext(_opt.Options))
             {
-                NodeRepository _db = new NodeRepository(ctxSave);
-                await _db.InsertNodeAsync(_node1);
+                NodeRepository _db = new NodeRepository(ctxSaveNode);
+                await _db.InsertNodeAsync(_node2);
             }
-            using (var ctxRead = new DataContext(_opt.Options))
+            using (var ctxReadNode = new DataContext(_opt.Options))
             {
                 _node = (
-                    from n in ctxRead.Nodes
-                    where n.NodeName == "France"
+                    from n in ctxReadNode.Nodes
+                    where n.NodeName == "3"
                     select n
                     ).FirstOrDefault();
             }
 
-            Vector _vector = new Vector
+            Vector _vector1 = new Vector
             {
                 VectorPhrase = "Gross Domestic Product",
+                NodeSubject = _node.NodeId
+            };
+            Vector _vector2 = new Vector
+            {
+                VectorPhrase = "in 2017",
                 NodeSubject = _node.NodeId
             };
             using (var ctxSave = new DataContext(_opt.Options))
             {
                 VectorRepository _db = new VectorRepository(ctxSave);
-                await _db.InsertVectorAsync(_vector);
+                await _db.InsertVectorAsync(_vector1);
+                await _db.InsertVectorAsync(_vector2);
             }
 
             //act
@@ -127,8 +163,195 @@ namespace UnionAll.Store.Test
                     ).ToList().Count();
             }
             //assert
-            Assert.AreEqual(1, _vectorCount);
+            Assert.AreEqual(2, _vectorCount);
+        }
+
+        [TestMethod]
+        public async Task InsertAndSelectTheSameSetOfVectors()
+        {
+            Node _node;
+            IEnumerable<Vector> _vectorSet = new List<Vector>();
+            IEnumerable<Vector> _savedVectors;
+            IEnumerable<Vector> _selectedVectors;
+            IEnumerable<int> _vectorIdList;
+
+            //arrange
+            using (var ctxSaveNode = new DataContext(_opt.Options))
+            {
+                NodeRepository _db = new NodeRepository(ctxSaveNode);
+                await _db.InsertNodeAsync(_node3);
+            }
+            using (var ctxReadNode = new DataContext(_opt.Options))
+            {
+                _node = (
+                    from n in ctxReadNode.Nodes
+                    where n.NodeName == "Belgium"
+                    select n
+                    ).FirstOrDefault();
+            }
+
+            Vector _vector3 = new Vector
+            {
+                VectorPhrase = "Is a Benelux country",
+                NodeSubject = _node.NodeId
+            };
+            _vectorSet.Append(_vector3);
+            _vectorSet.Append(_vector3);
+            Vector _vector4 = new Vector
+            {
+                VectorPhrase = "Has a name starting with a B",
+                NodeSubject = _node.NodeId
+            };
+            _vectorSet.Append(_vector4);
+            _vectorSet.Append(_vector4);
+            Vector _vector5 = new Vector
+            {
+                VectorPhrase = "Produces some chocolate",
+                NodeSubject = _node.NodeId
+            };
+            _vectorSet.Append(_vector5);
+            _vectorSet.Append(_vector5);
+
+            //act
+            using (var ctxSave = new DataContext(_opt.Options))
+            {
+                VectorRepository _db = new VectorRepository(ctxSave);
+                _savedVectors = await _db.InsertVectorSetAsync(_vectorSet);
+            }
+            _vectorIdList = _savedVectors.Select(x => x.VectorId);
+
+            using (var ctxReadSet = new DataContext(_opt.Options))
+            {
+                VectorRepository _db = new VectorRepository(ctxReadSet);
+                _selectedVectors = await _db.SelectVectorSetAsync(_vectorIdList);
+            }
+
+            //assert
+            Assert.AreEqual(_savedVectors.Count(), _vectorSet.Count(),
+                "The ceated vectors is not the same number as those returned from the save method");
+            Assert.AreEqual(_selectedVectors.Count(), _savedVectors.Count(),
+                "The selected vector set is not the same count as the inserted vector set");
+        }
+
+        [TestMethod]
+        public async Task UpdateOneVector()
+        {
+            Node _node;
+            Vector _vectorToUpdate;
+            Vector _updatedVector;
+
+            // arrange
+            using (var ctxSave = new DataContext(_opt.Options))
+            {
+                NodeRepository _db = new NodeRepository(ctxSave);
+                await _db.InsertNodeAsync(_node5);
+            }
+            using (var ctxReadNode = new DataContext(_opt.Options))
+            {
+                _node = (
+                    from n in ctxReadNode.Nodes
+                    where n.NodeName == "Employment"
+                    select n
+                    ).FirstOrDefault();
+            }
+
+            Vector _newVector = new Vector
+            {
+                VectorPhrase = "Not Updated",
+                VectorStatus = VectorStatusValues.Active,
+                NodeSubject = _node.NodeId,
+                NodeObject = 100,
+                NodeParent = 110,
+                NodeRoot = 120
+            };
+            using (var ctxSave = new DataContext(_opt.Options))
+            {
+                VectorRepository _db = new VectorRepository(ctxSave);
+                _vectorToUpdate = await _db.InsertVectorAsync(_newVector);
+            }
+
+            // act
+            _vectorToUpdate.HasEdits = true;
+            _vectorToUpdate.VectorPhrase = "Updated";
+            _vectorToUpdate.NodeObject = 200;
+            _vectorToUpdate.NodeParent = 210;
+
+            using (var ctxUpdate = new DataContext(_opt.Options))
+            {
+                VectorRepository _db = new VectorRepository(ctxUpdate);
+                await _db.UpdateVectorAsync(_vectorToUpdate);
+            }
+
+            using (var ctxReadVector = new DataContext(_opt.Options))
+            {
+                _updatedVector = (
+                    from v in ctxReadVector.Vectors
+                    where v.VectorId == _vectorToUpdate.VectorId
+                    select v
+                    ).FirstOrDefault();
+            }
+
+            // assert
+            Assert.AreEqual(_updatedVector.VectorPhrase, _vectorToUpdate.VectorPhrase);
+            Assert.AreNotEqual(_updatedVector.NodeObject, 100);
+            Assert.AreNotEqual(_updatedVector.NodeParent, 110);
+            Assert.AreEqual(_updatedVector.NodeRoot, _vectorToUpdate.NodeRoot);
+        }
+
+        [TestMethod]
+        public async Task DeleteOneVector()
+        {
+            Node _node;
+            Vector _activeVector;
+            Vector _deletedVector;
+            Vector _unretrievableVector;
+
+            //arrange
+            using (var ctxSave = new DataContext(_opt.Options))
+            {
+                NodeRepository _db = new NodeRepository(ctxSave);
+                await _db.InsertNodeAsync(_node6);
+            }
+            using (var ctxReadNode = new DataContext(_opt.Options))
+            {
+                _node = (
+                    from n in ctxReadNode.Nodes
+                    where n.NodeName == "Canada"
+                    select n
+                    ).FirstOrDefault();
+            }
+
+            Vector _newVector = new Vector
+            {
+                VectorPhrase = "Not Updated",
+                VectorStatus = VectorStatusValues.Active,
+                NodeSubject = _node.NodeId
+            };
+            using (var ctxSave = new DataContext(_opt.Options))
+            {
+                VectorRepository _db = new VectorRepository(ctxSave);
+                _activeVector = await _db.InsertVectorAsync(_newVector);
+            }
+
+            // act
+            _activeVector.VectorStatus = VectorStatusValues.Deleted;
+            _activeVector.HasEdits = true;
+
+            using (var ctxDelete = new DataContext(_opt.Options))
+            {
+                VectorRepository _db = new VectorRepository(ctxDelete);
+                _deletedVector = await _db.DeleteVectorAsync(_activeVector);
+            }
+            using (var ctxRead = new DataContext(_opt.Options))
+            {
+                VectorRepository _db = new VectorRepository(ctxRead);
+                _unretrievableVector = await _db.SelectVectorAsync(_node.NodeId, _deletedVector.VectorId);
+            }
+            // assert
+            Assert.IsNotNull(_activeVector);
+            Assert.AreEqual(VectorStatusValues.Deleted, _deletedVector.VectorStatus);
+            Assert.IsNull(_unretrievableVector);
         }
 
     }
-}
+ }
